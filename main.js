eea1cc7c27b97ff4067d84190a8764b479323e9c
Jan Walaszek <jan.walaszek@pm.me>
2023-07-18 22:15:06 +0200

    Add main part of fuctions for btns

--- main.js
+++ main.js
@@ -6,7 +6,15 @@ const previousDisplayValue = document.querySelector("#previous");
 const currentDisplayValue = document.querySelector("#current");
 
 const clear = document.querySelector("#btn-clear");
+clear.addEventListener("click", clearDisplay);
+
 const result = document.querySelector("#btn-result");
+result.addEventListener("click", () => {
+    if(currentValue != "" && previousValue != "") {
+        calculate();
+    }
+});
+
 const dot = document.querySelector("#btn-dot");
 
 const numberBtns = document.querySelectorAll(".numbers");
@@ -19,6 +27,10 @@ numberBtns.forEach((btn) => {
 });
 
 function handleNumber(number) {
+    if(previousValue !== "" && currentValue !== "" && operator === ""){
+        previousValue = "";
+        currentDisplayValue.textContent = currentValue;
+    }
     if (currentValue.length <= 6) {
     currentValue += number;
     currentDisplayValue.textContent = currentValue;
@@ -31,9 +43,71 @@ operators.forEach((btn) => {
     });
 });
 
-function handleOperator(operator) {
-    previousValue = currentValue;
+function handleOperator(op) {
+    if (previousValue === "") {
+        previousValue = currentValue;
+        operatorCheck(op);
+    } else if (currentValue === "") {
+        operatorCheck(op);
+    } else {
+        calculate();
+        operator = op;
+        currentDisplayValue.textContent = "0";
+        previousDisplayValue.textContent = previousValue + " " + operator;
+    }
+}
+
+function calculate() {
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    if (operator === "+") {
+        previousValue += currentValue;
+    } else if (operator === "-") {
+        previousValue -= currentValue;
+    } else if (operator === "/") {
+        if (currentValue <= 0) {
+            previousValue = "Error :(";
+            displayResult();
+            return;
+        }
+        previousValue /= currentValue;
+    } else if (operator === "x") {
+        previousValue *= currentValue
+    }  
+    
+    previousValue = round(previousValue);
+    previousValue = previousValue.toString();
+    displayResult();
+}
+
+function operatorCheck(text) {
+    operator = text;
     previousDisplayValue.textContent = previousValue + " " + operator;
+    currentDisplayValue.textContent = "0";
     currentValue = "";
-    currentDisplayValue.textContent = "";
+}
+
+function displayResult() {
+    if (previousValue.length <= 6) {
+        currentDisplayValue.textContent = previousValue;
+    } else {
+        currentDisplayValue.textContent = previousValue.slice(0,6) + "...";
+    }
+
+    previousDisplayValue.textContent = "";
+    operator = "";
+    currentValue = "";
+}
+
+function round(num) {
+    return Math.round(num * 100000) / 100000;
+  }
+
+function clearDisplay() {
+    previousDisplayValue.textContent = "";
+    previousValue = "";
+    currentDisplayValue.textContent = "0";
+    currentValue = "";
+    operator = "";
 }
